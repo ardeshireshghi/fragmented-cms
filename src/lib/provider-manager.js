de3f88e0e7b8962c15fb568de68cfa1d3c1d100c
Ardeshir Eshghi <ardeshir.eshghi@fundingcircle.com>
2019-03-07 13:18:58 +0000

    Refactor provider-manager params logic

--- src/lib/provider-manager.js
+++ src/lib/provider-manager.js
@@ -1,40 +1,35 @@
 const { createService } = require('./providers/factory');
 
-function serviceParams(backendName, config) {
-  if (backendName === 'git') {
-    return {
-      remoteOrigin: config.git.remote,
-      localPath: config.git.local
-    };
-  }
-
-  if (backendName === 's3') {
-    if (!config.s3 || !config.s3.bucket) {
-      throw new Error('S3 Bucket name can not be found in config');
-    }
-    return {
-      bucketName: config.s3.bucket
-    };
-  }
-
-  if (backendName === 'wordpress') {
-    if (!config.wordpress || !config.wordpress.url) {
-      throw new Error('Wordpress URL can not be found in config');
-    }
-    return {
-      url: config.wordpress.url
-    };
-  }
-
-  throw new Error(`${backendName} service is not implemented`);
-}
-
 exports.ServiceProviderManager = (config) => {
   const services = {};
+  const paramsProvider = {
+    git() {
+      return {
+        remoteOrigin: config.git.remote,
+        localPath: config.git.local
+      };
+    },
+    s3() {
+      if (!config.s3 || !config.s3.bucket) {
+        throw new Error('S3 Bucket name can not be found in config');
+      }
+      return {
+        bucketName: config.s3.bucket
+      };
+    },
+    wordpress() {
+      if (!config.wordpress || !config.wordpress.url) {
+        throw new Error('Wordpress URL can not be found in config');
+      }
+      return {
+        url: config.wordpress.url
+      };
+    }
+  };
 
   async function getService(backendName) {
     if (!(backendName in services)) {
-      services[backendName] = createService(backendName, serviceParams(backendName, config));
+      services[backendName] = createService(backendName, paramsProvider[backendName]());
 
       if (backendName === 'git') {
         await services[backendName].fetch();
